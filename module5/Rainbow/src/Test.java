760a598c57406e9fa77e433d24c7fecaa7ff1b7e
Kirill <schegol.kirill@gmail.com>
2021-05-30 17:35:05 +0300

    Rainbow

---
+++ module5/Rainbow/src/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String[] args) {
+        String text = "Каждый охотник желает знать, где сидит фазан";
+        String[] colorWords = text.split(",?\\s+");
+        String[] colorWords2 = new String[colorWords.length];
+        int j = colorWords2.length - 1;
+        for(int i = 0; i < colorWords.length; i++) {
+            colorWords2[j] = colorWords[i];
+            j--;
+        }
+        for(String word : colorWords2){
+            System.out.println(word);
+        }
+    }
+}
